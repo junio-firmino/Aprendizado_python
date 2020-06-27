33958aadce42ab599da1bad49794cf29b3715834
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2020-06-26 22:40:47 -0300

    Aprendizado do método enumerate

---
+++ enumerate_1.py
@@ -0,0 +1,5 @@
+# Aprendendo a função enumerate
+b = ['Abel','Jorge Jesus','Jovanildo','Luxemburgo','Mano Menezes']
+
+for i, n in enumerate(b,1): #i é o indice e o n é os itens da lista
+    print(str(i) +". " + n)
